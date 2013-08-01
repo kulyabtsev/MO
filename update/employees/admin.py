3a71b58aa36184a1f5c6d5cfef3ce443a4da8663
root <root@linuxtest.(none)>
2013-08-01 23:48:12 +0600

    First changes

--- update/employees/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from employees.models import Unit, Education, Jobtitle
-
-admin.site.register(Unit)
-admin.site.register(Education)
-admin.site.register(Jobtitle)
--- update/organizations/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from organizations.models import Organization, Branch
-
-admin.site.register(Organization)
-admin.site.register(Branch)
